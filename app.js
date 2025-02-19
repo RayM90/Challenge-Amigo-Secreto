d96e3d93bd0673e401e177b670e968954d7e7544
RaymarM <148508385+RayM90@users.noreply.github.com>
2025-02-18 22:31:19 -0400

    Se implemento la función para mostrar y actualizar la lista de amigos

--- app.js
+++ app.js
@@ -8,35 +8,41 @@ let amigos = [];
 // Funcion para agregar un nombre a la lista.
 
 function agregarAmigo () {
-
     /* Declaracion de variable "input" para almacenar el id
     document.getElementById('agregarAmigo'): busca el elemento*/
-
-
     const input = document.getElementById(`amigo`);
-
     //almacenará el valor limpio (sin espacios) que el usuario ha ingresado.
-
     const nombre = input.value.trim();
 
     // Validar la entrada.
-
     if (nombre == "") {
-
         alert("Por favor , inserte un nombre");
         return;
     }
 
     // Agregar, Actualizar el array
-
     amigos.push(nombre);
-
     //Limpiar el campo.
-
     input.value = "";
-
     // Mostrar lista
+    mostrarlistaAmigos();  
+}
 
-    mostrarlistaAmigos();
+//Función para mostrar la lista.
+
+function mostrarlistaAmigos(){
+    // Muestra el nombre de los amigos
+    const lista = document.getElementById(`listaAmigos`);
+    //Limpiar la lista
+    lista.innerHTML= "";
+
+    //Agregar a cada amigo como un elemento
+
+    for (let i = 0; i < amigos.length; i++){
+        const li = document.createElement(`li`);
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
     
 }
+
